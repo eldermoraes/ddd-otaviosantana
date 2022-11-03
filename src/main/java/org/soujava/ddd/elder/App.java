1db499cc14a489e38ebe369884d94dd8a07cec2d
Otavio Santana <otaviopolianasantana@gmail.com>
2022-11-03 17:23:52 +0000

    rename package
    Signed-off-by: Otavio Santana <otaviopolianasantana@gmail.com>

--- src/main/java/org/soujava/ddd/elder/App.java
+++ src/main/java/org/soujava/ddd/elder/App.java
@@ -1,4 +1,4 @@
-package org.example;
+package org.soujava.ddd.elder;
 
 /**
  * Hello world!
--- src/test/java/org/soujava/ddd/elder/AppTest.java
+++ src/test/java/org/soujava/ddd/elder/AppTest.java
@@ -1,4 +1,4 @@
-package org.example;
+package org.soujava.ddd.elder;
 
 
 import org.junit.jupiter.api.Test;

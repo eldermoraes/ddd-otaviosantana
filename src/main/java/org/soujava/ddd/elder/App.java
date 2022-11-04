829ae71abd8a0e90e6d6fbc44f1a55ff188753dc
Otavio Santana <otaviopolianasantana@gmail.com>
2022-11-04 07:24:22 +0000

    modify App
    Signed-off-by: Otavio Santana <otaviopolianasantana@gmail.com>

--- src/main/java/org/soujava/ddd/elder/App.java
+++ src/main/java/org/soujava/ddd/elder/App.java
@@ -2,12 +2,8 @@ package org.soujava.ddd.elder;
 
 /**
  * Hello world!
- *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+    public static void main(String[] args) {
     }
 }
